73f6851907a5bc8ef220fe9f275e922c9a47c205
Zidan Omar Hamid <zidanomar.hamid@vivavis.com.tr>
2022-11-10 17:55:22 +0300

    testing 🎐

--- assets/seed.sql
+++ assets/seed.sql
@@ -1,5 +1,5 @@
 -- (isbn, title, author, year_of_publication, publisher, image_url_s, image_url_m, image_url_l)
 
 COPY public."Book"(isbn, title, author, year_of_publication, publisher, image_url_s, image_url_m, image_url_l)
-FROM '/var/lib/postgresql/data/unique_books.csv'
+FROM '/var/lib/postgresql/data/seed/books.csv'
 CSV HEADER DELIMITER ';' QUOTE '"' ESCAPE '\';
