5877c03972a53ef3dcaf90a3ec31a61c562c45a2
Zidan Omar Hamid <hzidanomar@gmail.com>
2022-11-12 13:19:18 +0300

    cleaning up redundant data 🚲

--- assets/seed.sql
+++
@@ -1,5 +0,0 @@
--- (isbn, title, author, year_of_publication, publisher, image_url_s, image_url_m, image_url_l)
-
-COPY public."Book"(isbn, title, author, year_of_publication, publisher, image_url_s, image_url_m, image_url_l)
-FROM '/var/lib/postgresql/data/seed/books.csv'
-CSV HEADER DELIMITER ';' QUOTE '"' ESCAPE '\';
