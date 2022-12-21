42b2bd519877998b0930295377f322bbf61b4e5b
Zidan Omar Hamid <zidanomar.hamid@vivavis.com.tr>
2022-12-21 21:33:12 +0300

    update cors settings 🍳

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,12 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://bookshelf-client.onrender.com/',
+      'https://tasarim.430am.dev',
+    ],
     credentials: true,
   });
 
